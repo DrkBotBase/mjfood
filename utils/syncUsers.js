7f145b4429032bc712044f4a9e97055726955240
DrkBotBase <marziosincro@gmail.com>
2025-11-12 18:00:27 -0500

    nuevo restaurante y arreglos

--- utils/syncUsers.js
+++ utils/syncUsers.js
@@ -12,7 +12,7 @@ async function sincronizarUsuarios() {
       if (restauranteId === 'demo') continue;
       const existingUser = await User.findOne({ restauranteId });
       if (!existingUser) {
-        const tempPassword = Math.random().toString(36).slice(-8);
+        const tempPassword = 'user1234';//Math.random().toString(36).slice(-8);
         const hashedPassword = await bcrypt.hash(tempPassword, 10);
         const newUser = new User({
           username: restauranteId,
