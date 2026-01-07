a668c645aa98f7236405fa643beeb4e404e9af7d
DrkBotBase <marziosincro@gmail.com>
2026-01-07 00:46:40 -0500

    test notify

--- routes/push.routes.js
+++ routes/push.routes.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const PushSubscription = require("../models/Subscription");
 
 router.post("/subscribe", async (req, res) => {
+  console.log("📥 SUBSCRIBE BODY:", req.body);
+
   try {
     const { endpoint, keys } = req.body;
 
     if (!endpoint || !keys) {
+      console.log("❌ Datos inválidos");
       return res.status(400).json({ message: "Datos inválidos" });
     }
 
@@ -16,8 +19,11 @@ router.post("/subscribe", async (req, res) => {
       { upsert: true }
     );
 
+    console.log("✅ Suscripción guardada");
     res.status(201).json({ success: true });
+
   } catch (error) {
+    console.error("🔥 ERROR:", error);
     res.status(500).json({ error: "Error al guardar suscripción" });
   }
 });
