c21deb75560ec6a1464548ca98934a4f8d9e1a51
DrkBotBase <marziosincro@gmail.com>
2026-01-14 00:06:43 -0500

    fix sitemap and robots

--- routes/seo.js
+++ routes/seo.js
@@ -2,34 +2,65 @@ const express = require('express');
 const router = express.Router();
 const { info, PORT } = require('../config');
 
-const rutasSitemap = [
-    { url: '/', lastmod: '2026-01-11', priority: '1.0' },
-    { url: '/lista', lastmod: '2026-01-11', priority: '0.7' },
-    //{ url: '/contacto', lastmod: '2026-01-11', priority: '0.8' },
-];
+const { cargarMenusDesdeArchivos } = require('../utils/recargarMenus');
 
-router.get('/sitemap.xml', (req, res) => {
-    let xml = `<?xml version="1.0" encoding="UTF-8"?>`;
-    xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
+router.get('/sitemap.xml', async (req, res) => {
+  try {
+    const menus = await cargarMenusDesdeArchivos();
+    const extensiones = Object.keys(menus);
 
-    rutasSitemap.forEach(ruta => {
-        xml += `
-        <url>
-            <loc>${info.dominio}${ruta.url}</loc>
-            <lastmod>${ruta.lastmod}</lastmod>
-            <priority>${ruta.priority}</priority>
-        </url>`;
+    const fechaHoy = new Date().toISOString().split('T')[0];
+
+    let xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://mjfood.top/</loc>
+    <lastmod>${fechaHoy}</lastmod>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://mjfood.top/lista</loc>
+    <lastmod>${fechaHoy}</lastmod>
+    <priority>0.8</priority>
+  </url>`;
+
+    extensiones.forEach(slug => {
+      xml += `
+  <url>
+    <loc>https://mjfood.top/${slug}</loc>
+    <lastmod>${fechaHoy}</lastmod>
+    <priority>0.7</priority>
+  </url>`;
     });
 
-    xml += `</urlset>`;
+    xml += `\n</urlset>`;
 
     res.header('Content-Type', 'application/xml');
     res.send(xml);
+
+  } catch (error) {
+    console.error("Error generando sitemap:", error);
+    res.status(500).end();
+  }
 });
+;
 
 router.get('/robots.txt', (req, res) => {
     res.type('text/plain');
-    res.send(`User-agent: *\nAllow: /\nSitemap: ${info.dominio}/sitemap.xml`);
+    res.send(
+`User-agent: *
+Allow: /
+Allow: /assets/
+Allow: /css/
+Allow: /js/
+Disallow: /admin/
+Disallow: /login/
+Disallow: /api/
+Disallow: /temp/
+
+Sitemap: ${info.dominio}/sitemap.xml`
+    );
 });
+
 
 module.exports = router;
